6e5a12887d1c3c62ce84fc3b4d42d09601ab2a7d
codingkrishna <codingkrishna@gmail.com>
2023-09-20 21:16:14 +0530

    docker file added

--- src/main/java/com/bizlog/rms/api/impl/OrganizationResource.java
+++ src/main/java/com/bizlog/rms/api/impl/OrganizationResource.java
@@ -10,7 +10,6 @@ import com.bizlog.rms.service.OrganizationService;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
