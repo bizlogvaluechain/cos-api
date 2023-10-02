d4494a6edde9d897bbe0831ab6c8c56532dc5096
codingkrishna <codingkrishna@gmail.com>
2023-10-02 22:46:37 +0530

    removed db schema file

--- src/main/resources/db-schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS customer (
-    id SERIAL PRIMARY KEY,
-    `name` VARCHAR(45),
-    `email` VARCHAR(45),
-    `city` VARCHAR(45),
-    `street` VARCHAR(45),
-    `number` VARCHAR(45)
-);
