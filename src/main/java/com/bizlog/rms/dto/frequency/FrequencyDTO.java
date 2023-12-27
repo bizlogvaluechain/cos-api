6b6d4fd59d489154fe7a597c641634dac8fdb84c
dev4akashgupta <dev4akashgupta@gmail.com>
2023-12-27 17:47:52 +0530

    Change in field of Local date and time as Epoch Time

--- src/main/java/com/bizlog/rms/dto/frequency/FrequencyDTO.java
+++ src/main/java/com/bizlog/rms/dto/frequency/FrequencyDTO.java
@@ -4,15 +4,14 @@ import com.bizlog.rms.dto.BaseDTO;
 import com.bizlog.rms.entities.frequency.HolidayApplicable;
 import lombok.Data;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
 public class FrequencyDTO extends BaseDTO {
     private String ticketsVolume;
-    private LocalDateTime onboardingDate;
-    private LocalDateTime dayStartTime;
-    private LocalDateTime dayEndTime;
+    private Long onboardingDate;
+    private Long dayStartTime;
+    private Long dayEndTime;
     private List<HolidayApplicable> holidayApplicable;
     private Boolean onlyWorkdays;
 }
--- src/main/java/com/bizlog/rms/entities/frequency/Frequency.java
+++ src/main/java/com/bizlog/rms/entities/frequency/Frequency.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.List;
 
 @Entity
@@ -13,15 +14,33 @@ import java.util.List;
 public class Frequency extends BaseClientEntity {
     @Column(name = "ticketsVolume", nullable = false)
     private String ticketsVolume;
+
     @Column(name = "onboardingDate", nullable = false)
-    private LocalDateTime onboardingDate;
+    private long onboardingDate;
+
     @Column(name = "dayStartTime", nullable = false)
-    private LocalDateTime dayStartTime;
+    private long dayStartTime;
+
     @Column(name = "dayEndTime", nullable = false)
-    private LocalDateTime dayEndTime;
+    private long dayEndTime;
+
     @Column(name = "holidayApplicable", nullable = false)
     @ElementCollection(targetClass = HolidayApplicable.class, fetch = FetchType.EAGER)
     private List<HolidayApplicable> holidayApplicable;
+
     @Column(name = "onlyWorkdays", nullable = false)
     private Boolean onlyWorkdays;
+
+
+    @PrePersist
+    @PreUpdate
+    private void updateEpochTimes() {
+        onboardingDate = convertToEpochSeconds(LocalDateTime.ofEpochSecond(onboardingDate, 0, ZoneOffset.UTC));
+        dayStartTime = convertToEpochSeconds(LocalDateTime.ofEpochSecond(dayStartTime, 0, ZoneOffset.UTC));
+        dayEndTime = convertToEpochSeconds(LocalDateTime.ofEpochSecond(dayEndTime, 0, ZoneOffset.UTC));
+    }
+
+    private long convertToEpochSeconds(LocalDateTime localDateTime) {
+        return localDateTime.toEpochSecond(ZoneOffset.UTC);
+    }
 }
