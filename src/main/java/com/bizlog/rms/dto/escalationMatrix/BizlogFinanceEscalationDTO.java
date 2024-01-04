edc3d5b90888e27725f60b6d26019bc95550abe6
dev4akashgupta <dev4akashgupta@gmail.com>
2024-01-04 18:08:24 +0530

    changeing as per the new

--- src/main/java/com/bizlog/rms/dto/escalationMatrix/BizlogFinanceEscalationDTO.java
+++ src/main/java/com/bizlog/rms/dto/escalationMatrix/BizlogFinanceEscalationDTO.java
@@ -1,7 +1,17 @@
 package com.bizlog.rms.dto.escalationMatrix;
 
+import com.bizlog.rms.dto.BaseDTO;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-public class BizlogFinanceEscalationDTO extends EscalationMatrixDTO {
+@NoArgsConstructor
+@AllArgsConstructor
+public class BizlogFinanceEscalationDTO extends BaseDTO {
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private String mobile;
+    private String designation;
 }
