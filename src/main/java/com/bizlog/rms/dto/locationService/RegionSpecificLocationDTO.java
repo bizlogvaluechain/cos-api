379bac7ffb538d4a3640d405cb97e6335857dd72
dev4akashgupta <dev4akashgupta@gmail.com>
2024-02-02 17:07:49 +0530

    adding some field Location page and entity

---
+++ src/main/java/com/bizlog/rms/dto/locationService/RegionSpecificLocationDTO.java
@@ -0,0 +1,17 @@
+package com.bizlog.rms.dto.locationService;
+
+import com.bizlog.rms.dto.BaseDTO;
+import lombok.Data;
+
+@Data
+public class RegionSpecificLocationDTO extends BaseDTO {
+    private String country;
+    private  String state;
+    private String City;
+    private  String address1;
+    private String address2;
+    private String pinCode;
+    private String email;
+    private String contact1;
+    private String contact2 ;
+}
