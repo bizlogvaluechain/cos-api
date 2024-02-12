f48bb353092e88ba2f4567d943481ed638ac2247
dev3sunil <dev3sunil@gmail.com>
2024-02-12 22:51:26 +0530

    Cors issue fix.

--- src/main/java/com/bizlog/rms/config/CorsConfig.java
+++ src/main/java/com/bizlog/rms/config/CorsConfig.java
@@ -11,8 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**").allowedOrigins("http://localhost:3000")
-//                .allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*").allowCredentials(true);
-        registry.addMapping("/**").allowedMethods("*");
+        registry.addMapping("/**").allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*").allowCredentials(true);
     }
 }
