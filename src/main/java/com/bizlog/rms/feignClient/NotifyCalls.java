22abba17d322b5d397c36331f72d1f222b3e15d1
sunil <dev3sunil@gmail.com>
2024-03-20 17:09:18 +0530

    issues solved for organization creation

--- src/main/java/com/bizlog/rms/feignClient/NotifyCalls.java
+++ src/main/java/com/bizlog/rms/feignClient/NotifyCalls.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "org-api", url = "http://localhost:9081", path = "/api")
+@FeignClient(name = "org-api", url = "http://localhost:8089", path = "/api")
 public interface NotifyCalls {
     @PostMapping("/email/registration")
     String postOrg(@RequestBody NotifyDTO notifyDTO);
